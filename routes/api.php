966f3f10bda08f1285ce34a16fdd05897959a9d9
shukeel <triggaillest@gmail.com>
2023-03-16 16:49:24 +0300

    modfied

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
@@ -17,6 +19,17 @@ use Laravel\Fortify\Http\Controllers\RegisteredUserController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/product/{product:id}', [ProductController::class, 'show']);
+
+Route::prefix('/cart')->group(function () {
+    Route::get('/', [CartController::class, 'index']);
+    Route::post('/add/{product:id}', [CartController::class, 'add']);
+    Route::delete('/remove/{product:id}', [CartController::class, 'remove']);
+    Route::put('/updated-quantity/{product:id}', [CartController::class, 'updateQuantity']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
